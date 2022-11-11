c74bb0f93045609a708aba8c08302f16f80ea318
matolgithub <89175955650@mail.ru>
2022-11-11 20:59:05 +0300

    Next commit to Git.

--- main.py
+++ main.py
@@ -1,11 +1,51 @@
+import os
+from os import getenv
+from dotenv import load_dotenv
 import requests
 from bs4 import BeautifulSoup
-from fake_user_agent import user_agent
+from fake_useragent import UserAgent
 from fake_headers import Headers
+import random
+from pprint import pprint
+
+load_dotenv()
+session = requests.Session()
+
+# not successfully
+# url_login = "https://ticket.pobeda.aero/websky/json/login"
+# profile_url = "https://ticket.pobeda.aero/websky/#/private/profile"
+
+url_login = "https://shop220.ru/index.php"
+profile_url = "https://shop220.ru/?action=cabinet"
+
+ua = UserAgent().random
+# print(f"USER-AGENT:   {ua}")
+
+header = {
+    "user-agent": ua
+}
+
+# headers = Headers()
+# header = headers.generate()
+
+# data = {
+# "login": getenv("POBEDA_LOGIN"),
+# "password": getenv("POBEDA_PASSWORD")
+# }
+
+data = {
+    "action": getenv("SHOP220_ACTION"),
+    "keepoldcart": getenv("SHOP220_KEEPOLDCART"),
+    "Username": getenv("SHOP220_LOGIN"),
+    "Password": getenv("SHOP220_PASSWORD")
+}
 
 
 def main():
-    pass
+    response = session.post(url=url_login, data=data, headers=header)
+    print(response.status_code)
+    print(response.text)
+    # print(response.json())
 
 
 if __name__ == "__main__":
