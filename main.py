48b68c874f9a946d10ed9c72c5520bf118a23f5a
matolgithub <89175955650@mail.ru>
2022-11-11 15:05:38 +0300

    First commit, start new project.

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+from fake_user_agent import user_agent
+from fake_headers import Headers
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
